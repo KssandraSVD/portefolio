a6fb01029f0d8ab994ecf82a4e163d25ba356d5a
Cassandra Sciauvaud <cassandra.sciauvaud@hotmail.fr>
2022-10-17 09:56:29 +0200

    123

--- php/contact.php
+++ php/contact.php
@@ -1,61 +1,55 @@
- <?php 
+<?php
+    $email = $nom = $prenom = $message = $emailError = $nomError = $prenomError = $messageError = "";
+
+    if ( isset ($_POST['submit'])){
+        $email = $_POST['email'];
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $message = $_POST['message'];
+    }
 
 
-//Import PHPMailer class into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
 
-$msg = '';
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
 
-if (array_key_exists('email', $_POST)) {
-    date_default_timezone_set('Etc/UTC');
+    require_once "../vendor/autoload.php";
+    $mail = new PHPMailer(true);
 
-    require '../vendor/autoload.php';
-
-    //Create a new PHPMailer instance
-    $mail = new PHPMailer();
-    //Send using SMTP to localhost (faster and safer than using mail()) – requires a local mail server
-    //See other examples for how to use a remote server such as gmail
+    try {
+     
+    // Autentification avec SMTP
     $mail->isSMTP();
+    $mail->SMTPAuth = true;
+
+    // Connexion
     $mail->Host = 'smtp.hostinger.com';
     $mail->Port = 465;
+    $mail->Username = 'webmaster@cassandra-svd.fr';
+    $mail->Password = 'Depourvue83@';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
 
-    //Use a fixed address in your own domain as the from address
-    //**DO NOT** use the submitter's address here as it will be forgery
-    //and will cause your messages to fail SPF checks
-    $mail->setFrom('webmaster@cassandra-svd.fr');
-    //Choose who the message should be sent to
-    //You don't have to use a <select> like in this example, you can simply use a fixed address
-    //the important thing is *not* to trust an email address submitted from the form directly,
-    //as an attacker can substitute their own and try to use your form to send spam
+    // Destinataires de l'email
+    $mail->setFrom('webmaster@cassandra-svd.fr', 'Cassandra');
     $mail->addAddress('cassandra.sciauvaud@hotmail.fr');
-    //Put the submitter's address in a reply-to header
-    //This will fail if the address provided is invalid,
-    //in which case we should ignore the whole request
-    if ($mail->addReplyTo($_POST['email'], $_POST['nom'], $_POST['prenom'])) {
-        $mail->Subject = 'PHPMailer contact form';
-        //Keep it simple - don't use HTML
-        $mail->isHTML(false);
-        //Build a simple message body
-        $mail->Body = <<<EOT
-            Email: {$_POST['email']}
-            Name: {$_POST['nom']}
-            Prenom: {$_POST['prenom']}
-            Message: {$_POST['message']}
-            EOT;
-        //Send the message, check for errors
-        if (!$mail->send()) {
-            //The reason for failing to send will be in $mail->ErrorInfo
-            //but it's unsafe to display errors directly to users - process the error, log it on your server.
-            $msg = 'Sorry, something went wrong. Please try again later.';
-        } else {
-            $msg = 'Message sent! Thanks for contacting us.';
-        }
-    } else {
-        $msg = 'Invalid email address, message ignored.';
-    }
+    
+    // Ajout du contenue de l'email
+    $mail->CharSet = 'UTF-8';
+    $mail->Encoding = 'base64';
+    $mail->isHTML(false);
+    $mail->Subject = 'Mail de votre site';
+    $mail->Body = $_POST['email'] . $_POST['nom']  . $_POST['message'];
+    
+
+    // Envoi de l'email
+    $mail->send();
+    echo 'Message envoyé';
+} catch (Exception $e) {
+    echo $e->errorMessage(); //error messages from PHPMailer
+} catch (\Exception $e) { //The leading slash means the Global PHP Exception class will be caught
+    echo $e->getMessage(); //Boring error messages from anything else!
 }
-  
-   
-?> 
 
-
+?>
